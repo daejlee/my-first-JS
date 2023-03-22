e3e613f99cac35b5a9bf6403ec66e98e13b4c73f
daejin <savif4991@gmail.com>
2023-03-22 14:57:03 +0900

    problem02 solved

--- woowa/02/problem2.js
+++ woowa/02/problem2.js
@@ -1,16 +1,52 @@
 /*
 REQUIRED FEATURES
-1. receive cryptogram and parse, if it violates the restrictions then return NULL.
+1. receive cryp and parse, if it violates the restrictions then return NULL.
 2. delete duplicated characters reversely, and return
 */
 
-function problem2(cryptogram)
+function removal_iter(cryp)
 {
 	let i = 0;
-	while (cryptogram[i])
+	let k;
+	while (i < cryp.length)
 	{
-		if (cryptogram[i] > 'z' || cryptogram[i++] < 'a')
+		if (cryp[i] === cryp[i + 1])
+		{
+			k = 0;
+			while (cryp[i + k] === cryp[i + k + 1])
+				k++;
+			cryp.splice(i, k + 1);
+			i = 0;
+		}
+		else
+			i++;
+	}
+	return (cryp);
+}
+
+function remove_dup(cryp)
+{
+	cryp = cryp.split("");
+	cryp = cryp.reverse();
+
+	cryp = removal_iter(cryp);
+
+	cryp = cryp.reverse();
+	cryp = cryp.join("");
+	return (cryp);
+}
+
+function problem2(cryp)
+{
+	let i = 0;
+	if (cryp.length > 1000)
+		return (null);
+	while (cryp[i])
+	{
+		if (cryp[i] > 'z' || cryp[i++] < 'a')
 			return (null);
 	}
-	console.log(cryptogram);
+	return (remove_dup(cryp));
 }
+
+module.exports = problem2;
