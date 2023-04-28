a1fe143d91465fb810d99d11dc59ab32b9f3d5d7
Daejin Lee <daejlee@c4r5s8.42seoul.kr>
2023-04-29 03:26:41 +0900

    4 challenge updated

---
+++ JS_challenge/4/challenge/app.js
@@ -0,0 +1,21 @@
+
+const numbers = document.querySelector("#numbers");
+const maxNumber = numbers.querySelector("#max-number");
+const inputNumber = numbers.querySelector("#input-number");
+
+
+function play(event)
+{
+	const randomNumber = Math.floor(Math.random() * (Number(maxNumber.value) + 1));
+	const result = document.querySelector("#result");
+	const inputNumberValue = Number(inputNumber.value);
+	event.preventDefault();
+	result.innerText = `You chose: ${inputNumberValue}, the machine chose: ${randomNumber}.`;
+	if (inputNumberValue === randomNumber)
+		document.querySelector("#result-message").innerText = "You Win!";
+	else
+		document.querySelector("#result-message").innerText = "You lost!";
+	result.classList.remove("hidden");
+}
+
+numbers.addEventListener("submit", play);
