ca6b96e74e841bfc8370c208ec1ff19b9e6b78e1
Daejin Lee <daejlee@c6r7s6.42seoul.kr>
2023-05-12 21:40:51 +0900

    cripto challenge added

--- React/7/react-for-beginners/src/App.js
+++ React/7/react-for-beginners/src/App.js
@@ -3,6 +3,18 @@ import { useEffect, useState } from "react";
 function App() {
 	const [loading, setLoading] = useState(true);
 	const [coins, setCoins] = useState([]); //give default value! if don't it will be undefined
+	const [inputUSD, setInputUSD] = useState("");
+	const [coinUSD, setCoinUSD] = useState(0);
+	const [myCoins, setMyCoins] = useState(0);
+	const onCoinChange = (event) => {
+		setCoinUSD(JSON.parse(event.target.value).quotes.USD.price);
+	};
+	const onInputUSDChange = (event) => setInputUSD(event.target.value);
+	const onSubmit = (event) =>{
+		event.preventDefault();
+		setMyCoins(coinUSD / inputUSD);
+		setInputUSD("");
+	}
 	useEffect(() => {
 		fetch("https://api.coinpaprika.com/v1/tickers")
 		.then((response) => response.json())
@@ -15,13 +27,25 @@ function App() {
 	<div>
 		<h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
 		{loading ? <strong>Loading</strong> : 
-		<select>
+		<select onChange={onCoinChange}>
+			<option value="xx">Select your cripto</option>
 			{coins.map((coin) => (
-				<option>
+				<option value={JSON.stringify(coin)}>
 					{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
 				</option>
 			))}
 		</select>}
+		<hr></hr>
+		<h1>How much cripto can I get? 🥺</h1>
+		<form onSubmit={onSubmit}>
+			<input
+			onChange={onInputUSDChange}
+			value={inputUSD}
+			type="text"
+			placeholder='input USD'
+			/>
+		</form>
+		<h2>{myCoins} cripto</h2>
 	</div>
 	);
 }
